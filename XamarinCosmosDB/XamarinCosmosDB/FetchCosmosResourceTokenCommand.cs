8415eb6bbd3c2f78a2a30579878ff21c7295364b
InquisitorJax <inquisitorjax@gmail.com>
2020-08-04 09:53:36 +0200

    some mobile app init code

---
+++ XamarinCosmosDB/XamarinCosmosDB/FetchCosmosResourceTokenCommand.cs
@@ -0,0 +1,35 @@
+﻿using CosmosResourceTokenProvider;
+using Wibci.LogicCommand;
+
+namespace XamarinCosmosDB
+{
+	public interface IFetchCosmosResourceTokenCommand : IAsyncLogicCommand<CosmosResourceTokenRequest, CosmosResourceTokenResult>
+	{
+	}
+
+	public class FetchCosmosResourceTokenCommand
+	{
+	}
+
+
+	public class CosmosResourceTokenResult : CommandResult
+	{
+		private CustomPermissionResponse tokenResponse;
+
+		public CustomPermissionResponse TokenResponse
+		{
+			get => tokenResponse ?? new CustomPermissionResponse();
+			set => tokenResponse = value;
+		}
+	}
+
+	public class CosmosResourceTokenRequest
+	{
+		public CosmosResourceTokenRequest(string authAccessToken)
+		{
+			AuthAccessToken = authAccessToken;
+		}
+		public string AuthAccessToken { get; }
+	}
+
+}

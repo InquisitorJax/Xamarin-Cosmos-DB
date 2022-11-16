ae9691e37fb941383528663b130573b3ca23f256
PirateMalcolm <malcolm@indiepirates.com>
2022-11-16 23:11:51 +0200

    working delete all items using partition key

--- CosmosResourceTokenProvider/Repositories/DataRepository.cs
+++ CosmosResourceTokenProvider/Repositories/DataRepository.cs
@@ -11,13 +11,13 @@ namespace CosmosResourceTokenProvider.Repositories
 
 	internal class DataRepository : IDataRepository
 	{
-		private readonly ILogger _log;
-		public DataRepository(ILogger log)
+		private readonly ILogger<IDataRepository> _log;
+		public DataRepository(ILogger<IDataRepository> log)
 		{
 			_log = log;
 		}
 
-		public Task DeleteAllAsync(string userId)
+		public async Task DeleteAllAsync(string userId)
 		{
 			// doc: https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/how-to-delete-by-partition-key?tabs=dotnet-example
 			// still in preview: https://github.com/AzureCosmosDB/PartitionKeyDeleteFeedbackGroup
@@ -25,17 +25,15 @@ namespace CosmosResourceTokenProvider.Repositories
 			var cosmosClientResult = CosmosClientFactory.Create();
 			var container = cosmosClientResult.Container;
 
-			var response = container.DeleteAllItemsByPartitionKeyStreamAsync(new Microsoft.Azure.Cosmos.PartitionKey(userId));
-			if (!response.IsCompletedSuccessfully)
+			var response = await container.DeleteAllItemsByPartitionKeyStreamAsync(new Microsoft.Azure.Cosmos.PartitionKey(userId));
+			if (!response.IsSuccessStatusCode)
 			{
-				_log.LogError($"Whoops! Something went wrong trying to delete the partition data for {userId}");
+				_log.LogError($"Whoops! Something went wrong trying to delete the partition data for {userId}. Status{response.StatusCode}");
 			}
 			else
 			{
 				_log.LogInformation($"Delete all documents with partition key operation has successfully started for {userId}");
 			}
-
-			return Task.CompletedTask;
 		}
 	}
 }
